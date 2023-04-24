55ca185cec4298e583518dd0eaa55499dc28809d
keenlabi <keenlabinc@gmail.com>
2023-04-24 05:17:07 +0100

    feat(auth routes): created an auth router and a POST '/login' endpoint

---
+++ src/api/v1/routes/authRouter.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const authRouter = Router();
+
+authRouter.post('/login')
+
+export default authRouter;
--- src/api/v1/routes/index.ts
+++ src/api/v1/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from "express"
+import authRouter from "./authRouter";
 
 export default function routes (){
     const router = Router();
+
+    router.use('/auth', authRouter)
 
     return router
 }
