a961232617a397a425402ba795a79822717e4f31
keenlabi <keenlabinc@gmail.com>
2023-04-25 20:04:17 +0100

    feat(password hash): hash request body plain text password with password utility function

--- src/api/v1/controllers/auth/registerStaff/registerStaff.ts
+++ src/api/v1/controllers/auth/registerStaff/registerStaff.ts
@@ -1,23 +1,26 @@
 import { Request, Response } from "express"
 import validateRegisterRequestBody from "./validateRegisterStaffRequestBody"
-import { hashPassword } from "../../../utils/authUtils/password"
+import { hashPassword } from "v1/utils/authUtils/password"
+import { UserModel } from "v1/models"
+import { sendFailureResponse, sendSuccessResponse } from "v1/utils/serverUtils/response"
 
 export default function registerStaff(req:Request, res:Response) {
     validateRegisterRequestBody(req.body)
-    .then((response)=> {
+    .then(({ requestBody })=> {
         // encrypt user password
-        hashPassword(req.body.password)
+        hashPassword(requestBody.password)
         .then((hashedPassword)=> {
-            console.log(req.body)
-            console.log(hashedPassword)
+            
         })
-        .catch(()=> {
-
+        .catch((error)=> {
+            console.log('There was an error hashing user password\n', error);
+            sendFailureResponse(res, 422, 'There was a server error, not your fault, try again');
         })
     })
     .catch((error)=> {
         // TODO: return error if validation is failed
-        console.log(`There was an error registering user`)
-        res.status(error.code).json({ message: error.message });
+        console.log(`VALIDATION ERROR: There was an error validating request body`)
+        console.log(error)
+        sendFailureResponse(res, error.code, error.message);
     })
 }
