9a33433064a385f9b58b4685c9b20e3d9fb30139
keenlabi <keenlabinc@gmail.com>
2023-04-25 19:10:30 +0100

    feat(routes endpoints): connected the registerStaff controller to the 'register-staff' endpoint

--- src/api/v1/routes/authRouter.ts
+++ src/api/v1/routes/authRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import registerStaff from "v1/controllers/auth/registerStaff/registerStaff";
 
 const authRouter = Router();
 
 authRouter.post('/login')
-authRouter.post('/register-staff')
+authRouter.post('/register-staff', registerStaff)
 
 export default authRouter;
