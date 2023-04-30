0e96dac36e87d2cfdb747b866f04a001c8a9da1c
keenlabi <keenlabinc@gmail.com>
2023-04-30 16:50:10 +0100

    feat(staff profile): created new route '/profile/:staffId' which connects to fetchStaffProfile controller

--- src/api/v1/routes/staffRouter.ts
+++ src/api/v1/routes/staffRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import fetchStaffProfile from "v1/controllers/staffs/fetchStaffProfile";
 import fetchStaffs from "v1/controllers/staffs/fetchStaffs";
 
 const staffRouter = Router();
 
+staffRouter.get('/profile/:staffId', fetchStaffProfile)
 staffRouter.get('/:pageNumber', fetchStaffs)
 
 export default staffRouter;
