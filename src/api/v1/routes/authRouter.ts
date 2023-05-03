dc3346464ff5863dd0755d348bb8e01df484048b
keenlabi <keenlabinc@gmail.com>
2023-05-03 03:11:04 +0100

    feat(staff routes): moved registerStaff endpoint from auth to staffs router.

--- src/api/v1/routes/authRouter.ts
+++ src/api/v1/routes/authRouter.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import login from "v1/controllers/auth/login/login";
-import registerStaff from "v1/controllers/auth/registerStaff/registerStaff";
 
 const authRouter = Router();
 
 authRouter.post('/login', login)
-authRouter.post('/register-staff', registerStaff)
 
 export default authRouter;
--- src/api/v1/routes/staffRouter.ts
+++ src/api/v1/routes/staffRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import registerStaff from "v1/controllers/staffs/registerStaff/registerStaff";
 import fetchStaffProfile from "v1/controllers/staffs/fetchStaffProfile";
 import fetchStaffs from "v1/controllers/staffs/fetchStaffs";
 
 const staffRouter = Router();
 
+staffRouter.post('/register', registerStaff)
 staffRouter.get('/profile/:staffId', fetchStaffProfile)
 staffRouter.get('/:pageNumber', fetchStaffs)
 
