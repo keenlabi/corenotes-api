d2f942b2a8a25205352be31376bd1069525a0cbf
keenlabi <keenlabinc@gmail.com>
2023-05-03 04:01:23 +0100

    feat(file uploader): added the uploadFile middleware to /staffs/register endpoint

--- src/api/v1/routes/staffRouter.ts
+++ src/api/v1/routes/staffRouter.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import registerStaff from "v1/controllers/staffs/registerStaff/registerStaff";
 import fetchStaffProfile from "v1/controllers/staffs/fetchStaffProfile";
 import fetchStaffs from "v1/controllers/staffs/fetchStaffs";
+import uploadFile from "v1/middlewares/uploadFile";
 
 const staffRouter = Router();
 
-staffRouter.post('/register', registerStaff)
+staffRouter.post('/register', uploadFile('single', 'profileImage'), registerStaff)
 staffRouter.get('/profile/:staffId', fetchStaffProfile)
 staffRouter.get('/:pageNumber', fetchStaffs)
 
