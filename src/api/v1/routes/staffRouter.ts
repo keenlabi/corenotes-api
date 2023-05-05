2f84212f3d33090b1a6224303ce2b9489e8bae05
keenlabi <keenlabinc@gmail.com>
2023-05-05 10:26:27 +0100

    feat(staff endpoints): created 2 new documents endpoints - GET '/:staffId/documents/:pageNumber' to fetch staff profile information; POST '/:staffId/documents' to upload staff documents to server

--- src/api/v1/routes/staffRouter.ts
+++ src/api/v1/routes/staffRouter.ts
@@ -3,11 +3,15 @@ import registerStaff from "v1/controllers/staffs/registerStaff/registerStaff";
 import fetchStaffProfile from "v1/controllers/staffs/fetchStaffProfile";
 import fetchStaffs from "v1/controllers/staffs/fetchStaffs";
 import uploadFile from "v1/middlewares/uploadFile";
+import uploadStaffDocument from "v1/controllers/staffs/documents/uploadStaffDocument/uploadStaffDocument";
+import fetchStaffDocuments from "v1/controllers/staffs/documents/fetchStaffDocuments";
 
 const staffRouter = Router();
 
 staffRouter.post('/register', uploadFile('single', 'profileImage'), registerStaff)
 staffRouter.get('/profile/:staffId', fetchStaffProfile)
+staffRouter.get('/:staffId/documents/:pageNumber', fetchStaffDocuments)
+staffRouter.post('/:staffId/documents', uploadFile('single', 'staffDocFile'), uploadStaffDocument)
 staffRouter.get('/:pageNumber', fetchStaffs)
 
 export default staffRouter;
