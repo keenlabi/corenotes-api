cc6862cb8403b446581e41b8f5fe19a0a0f1b87a
keenlabi <keenlabinc@gmail.com>
2023-05-06 21:54:23 +0100

    feat(staff routes): created two new routes POST '/:staffId/password-reset' & GET '/:staffId/deactivate'

--- src/api/v1/routes/staffRouter.ts
+++ src/api/v1/routes/staffRouter.ts
@@ -5,6 +5,8 @@ import fetchStaffs from "v1/controllers/staffs/fetchStaffs";
 import uploadFile from "v1/middlewares/uploadFile";
 import uploadStaffDocument from "v1/controllers/staffs/documents/uploadStaffDocument/uploadStaffDocument";
 import fetchStaffDocuments from "v1/controllers/staffs/documents/fetchStaffDocuments";
+import resetStaffPassword from "v1/controllers/staffs/security/resetStaffPassword";
+import deactivateStaff from "v1/controllers/staffs/security/deactivateStaff";
 
 const staffRouter = Router();
 
@@ -12,6 +14,9 @@ staffRouter.post('/register', uploadFile('single', 'profileImage'), registerStaf
 staffRouter.get('/profile/:staffId', fetchStaffProfile)
 staffRouter.get('/:staffId/documents/:pageNumber', fetchStaffDocuments)
 staffRouter.post('/:staffId/documents', uploadFile('single', 'staffDocFile'), uploadStaffDocument)
-staffRouter.get('/:pageNumber', fetchStaffs)
 
+staffRouter.post('/:staffId/password-reset', resetStaffPassword)
+staffRouter.get('/:staffId/deactivate', deactivateStaff)
+
+staffRouter.get('/:pageNumber', fetchStaffs)
 export default staffRouter;
