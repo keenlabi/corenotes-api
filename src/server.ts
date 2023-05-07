da85d42433d43df242c2a7333bc7b6eb51ec7ce8
keenlabi <keenlabinc@gmail.com>
2023-05-07 09:33:53 +0100

    feat(config): specified the allowed origins for server connection with cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 import express, { urlencoded } from "express"
 import { createServer } from "http"
 import routes from "./api/v1/routes";
+import cors from "cors";
 
 const app = express();
+
+const allowedOrigins = ['https://corenotes.netlify.app']
+const options: cors.CorsOptions = { origin: allowedOrigins, credentials: true }
+
+app.use(cors(options));
+
+app.set("trust proxy", true);
 
 // parses incoming JSON payloads
 app.use(express.json())
