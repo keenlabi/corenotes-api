3b6b796d75ce9fbb3e7f841c53489013fa6e3538
keenlabi <keenlabinc@gmail.com>
2023-05-07 06:01:58 +0100

    feat(types): export the Request extension of express made to accomodate the userId

--- src/types/request.d.ts
+++ src/types/request.d.ts
@@ -1,12 +1,21 @@
-declare global {
-    namespace Express {
-        interface Request {
-            userId?:string,
-            role?:string
-        }
+// declare global {
+//     namespace Express {
+//         interface Request {
+//             userId?:string,
+//             role?:string
+//         }
+//     }
+// }
+
+declare namespace Express {
+    export interface Request {
+        userId: any;
     }
-}
+    export interface Response {
+        userId: any;
+    }
+  }
 
 // If this file has no import/export statements (i.e. is a script)
 // convert it into a module by adding an empty export statement.
-export {}
+// export {}
