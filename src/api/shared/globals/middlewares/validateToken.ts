260f7bf09aa5b3f12a71a37aeafef155fe568f10
Keenlabi <keenlabinc@gmail.com>
2023-06-22 20:34:38 +0100

    refactor(shared services): refactored the shared services syntax

--- src/api/shared/globals/middlewares/validateToken.ts
+++ src/api/shared/globals/middlewares/validateToken.ts
@@ -5,59 +5,57 @@ import { NotAuthorizedError } from "../server/Error";
 import { getAuthUserByAuthAccessToken } from "../../services/db/auth.service";
 
 export default function validateToken (req:Request, res:Response, next:NextFunction) {
-    return new Promise((reject)=> {
-        let token:string = req.headers.cookie!;
+    let token:string = req.headers.cookie!;
 
-        if(token && token.includes('sid')) {
-            const allCookies = token.toString().split(';');
-            const tokenCookie = allCookies.filter(cookie => cookie.includes('sid'));
-            token = tokenCookie[0].split('=')[1];
+    if(token && token.includes('sid')) {
+        const allCookies = token.toString().split(';');
+        const tokenCookie = allCookies.filter(cookie => cookie.includes('sid'));
+        token = tokenCookie[0].split('=')[1];
 
-            
-            verify(token, process.env.JWT_KEY!, (error:any, decodedToken:any)=> {
-                if(error) {
-                    const unauthorizedError = new NotAuthorizedError('There was an error')
-                    return sendFailureResponse({ res, statusCode: unauthorizedError.statusCode, message: unauthorizedError.message });
-                }
+        
+        verify(token, process.env.JWT_KEY!, (error:any, decodedToken:any)=> {
+            if(error) {
+                const unauthorizedError = new NotAuthorizedError('There was an error')
+                return sendFailureResponse({ res, statusCode: unauthorizedError.statusCode, message: unauthorizedError.message });
+            }
 
-                getAuthUserByAuthAccessToken(decodedToken.id, token!)
-                .then((foundUser)=> {
-                    if(!foundUser) {
-                        const nonAuthorizedError = new NotAuthorizedError('Unauthorized')
-                        return sendFailureResponse({
-                            res, 
-                            statusCode: nonAuthorizedError.statusCode, 
-                            message: nonAuthorizedError.message
-                        });
-                    }
-                    
-                    req.currentUser = {
-                        id: foundUser.id,
-                        email: foundUser.email,
-                        firstname: foundUser.firstname
-                    }
-
-                    next();
-                })
-                .catch((error)=> {
-                    console.log(error)
+            getAuthUserByAuthAccessToken(decodedToken.id, token!)
+            .then((foundUser)=> {
+                if(!foundUser) {
                     const nonAuthorizedError = new NotAuthorizedError('Unauthorized')
                     return sendFailureResponse({
                         res, 
                         statusCode: nonAuthorizedError.statusCode, 
                         message: nonAuthorizedError.message
                     });
-                })
+                }
+                
+                req.currentUser = {
+                    id: foundUser.id,
+                    email: foundUser.email,
+                    firstname: foundUser.firstname
+                }
+
+                next();
             })
-    
-        } else {
-            console.log('token not found')
-            const nonAuthorizedError = new NotAuthorizedError('Unauthorized')
-            return sendFailureResponse({
-                res, 
-                statusCode: nonAuthorizedError.statusCode, 
-                message: nonAuthorizedError.message
-            });
-        }
-    });
+            .catch((error)=> {
+                console.log(error)
+                const nonAuthorizedError = new NotAuthorizedError('Unauthorized')
+                return sendFailureResponse({
+                    res, 
+                    statusCode: nonAuthorizedError.statusCode, 
+                    message: nonAuthorizedError.message
+                });
+            })
+        })
+
+    } else {
+        console.log('token not found')
+        const nonAuthorizedError = new NotAuthorizedError('Unauthorized')
+        return sendFailureResponse({
+            res, 
+            statusCode: nonAuthorizedError.statusCode, 
+            message: nonAuthorizedError.message
+        });
+    }
 }
