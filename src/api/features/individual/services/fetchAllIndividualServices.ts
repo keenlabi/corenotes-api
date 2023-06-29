99c0aa944c03b515357a62cdc9207fe170a453ca
Keenlabi <keenlabinc@gmail.com>
2023-06-29 14:44:51 +0100

    feat(individual services): created the fetch individual services controller function as well as the fetchAllIndividualServices service function

---
+++ src/api/features/individual/services/fetchAllIndividualServices.ts
@@ -0,0 +1,36 @@
+import { getServiceByObjectId } from "@services/db/service.service";
+import { IUser } from "@user/models/types";
+import userModel from "@user/models/user.model";
+
+export interface IIndividualServicesList {
+    id:string,
+    serviceId:number,
+    title:string,
+    startDate:string
+}
+
+export default function fetchAllIndividualServices(individualObjectId:string) {
+    return new Promise<IIndividualServicesList[]>((resolve, reject)=> {
+        const query = { _id: individualObjectId }
+
+        userModel.findOne(query)
+        .then(async (foundUser:IUser)=> {
+            const servicesDetails:IIndividualServicesList[] = [];
+            
+            for await ( const service of foundUser.requestedServices ) {
+                getServiceByObjectId(service.service)
+                .then((foundService)=> {
+                    servicesDetails.push({
+                        id: foundService._id.toString(),
+                        serviceId: foundService.serviceId,
+                        title: foundService.title,
+                        startDate: service.startDate
+                    })
+                })
+            }
+
+            resolve(servicesDetails)
+        })
+        .catch((error)=> reject(error))
+    })
+}
