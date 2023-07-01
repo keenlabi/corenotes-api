f457fac421751ebf614984e27a14b06ebdcec1f1
Keenlabi <keenlabinc@gmail.com>
2023-07-01 23:21:10 +0100

    feat(fetch staff list): mapped the staff list result to include the title of their respective compartments

--- src/api/features/staff/controllers/fetchStaffs.ts
+++ src/api/features/staff/controllers/fetchStaffs.ts
@@ -2,30 +2,50 @@ import { Request, Response } from "express"
 import UserModel from "@user/models/user.model"
 import { IUser } from "@user/models/types";
 import { sendFailureResponse, sendSuccessResponse } from "@globals/server/serverResponse";
+import { getCompartmentById } from "@services/db/compartment.service";
+
+interface staffList {
+    id: string; 
+    profileImage: string; 
+    firstname: string; 
+    lastname: string; 
+    dob: string; 
+    role:string;
+    gender: string; 
+    phoneNumber:string;
+    compartment: string; 
+    medicaidNumber: Number
+}
 
 export default function fetchStaffs(req:Request, res:Response) {
     const   pageNumber = parseInt(req.params.pageNumber) - 1 ?? 0,
             resultsPerPage = 10, 
             pageOffset = resultsPerPage * pageNumber;
 
-    const query = { role: 'STAFF' }
+    const query = { role: 'STAFF' };
 
     UserModel.find(query)
     .skip(pageOffset)
     .limit(resultsPerPage)
     .sort({ createdAt: -1 })
     .then(async (foundStaffs:IUser[])=> {
-        let filteredFormatStaff:any[] = foundStaffs.map((staff)=> {
-            return {
-                id: staff.id,
-                lastname: staff.lastname,
-                firstname: staff.firstname,
-                compartment: staff.compartment,
-                role: staff.role,
+
+        const mappedStaffs:Array<staffList>  = [];
+
+        for await ( const staff of foundStaffs ) {
+            mappedStaffs.push({
+                id:staff._id.toString(),
                 profileImage: staff.profileImage,
-                phoneNumber: staff.phoneNumber.work
-            }
-        })
+                firstname: staff.firstname,
+                lastname: staff.lastname,
+                dob: staff.dob,
+                role: staff.role,
+                phoneNumber: staff.phoneNumber.work,
+                gender: staff.gender,
+                compartment: (await getCompartmentById(staff.compartment)).title,
+                medicaidNumber: staff.medicaidNumber
+            })
+        }
 
         UserModel.count()
         .then((totalStaffCount:number)=> {
@@ -33,11 +53,11 @@ export default function fetchStaffs(req:Request, res:Response) {
             return sendSuccessResponse({res, statusCode: 200, message: "Staffs list retrieved successfully", data: { 
                 currentPage: parseInt(req.params.pageNumber), 
                 totalPages: totalPageNumber,
-                staffs: filteredFormatStaff
+                staffs: mappedStaffs
             }})
         })
     })
     .catch(()=> {
-        return sendFailureResponse({res, statusCode: 500, message: "There was an error fetching staff list"})
+        return sendFailureResponse({ res, statusCode: 500, message: "There was an error fetching staff list" })
     })
 }
