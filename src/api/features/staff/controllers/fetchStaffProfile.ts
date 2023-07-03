ee387f03bb2e9eb4063b5755e3acc8e6f3b29d54
Keenlabi <keenlabinc@gmail.com>
2023-07-03 19:53:41 +0100

    fix: corrected errors preventing build

--- src/api/features/staff/controllers/fetchStaffProfile.ts
+++ src/api/features/staff/controllers/fetchStaffProfile.ts
@@ -2,21 +2,17 @@ import { sendFailureResponse, sendSuccessResponse } from "@globals/server/server
 import { Request, Response } from "express"
 import UserModel from "@user/models/user.model"
 import { getStaffRoleById } from "@services/db/staff.service"
-import { IUser } from "@user/models/types"
 
 export default function fetchStaffProfile(req:Request, res:Response) {
 
     const query = { _id: req.params.staffId }
     
-    UserModel
-    .findOne(query)
-    .lean()
-    .then(async (foundStaff:IUser)=> {
-
+    UserModel.findOne(query)
+    .then(async (foundStaff)=> {
         const role = (await getStaffRoleById(foundStaff.providerRole)).title
         foundStaff.providerRole = role;
-
-        const { password, accessToken, createdAt, ...filteredFoundStaff } = foundStaff
+        
+        const { password, accessToken, createdAt, ...filteredFoundStaff } = foundStaff._doc
 
         return sendSuccessResponse({res, statusCode: 200, message: "Staff profile retrieved successfully", data: { 
             staff: filteredFoundStaff
